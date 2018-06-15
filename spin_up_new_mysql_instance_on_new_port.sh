a71688e946cd292eda94bc2a8532f96fe52b42f4
root <root@mysqltest1.vhwebdev.com>
2018-06-15 11:41:53 -0400

    fixed bug

--- spin_up_new_mysql_instance_on_new_port.sh
+++ spin_up_new_mysql_instance_on_new_port.sh
@@ -2,7 +2,7 @@
 echo "Currently, I see mysql running on ports:"
 ss -tulpen | grep mysql | awk '{print $5;}' | cut -d":" -f 4 | sort -n
 ID=$((`ss -tulpen | grep mysql | awk '{print $5;}' | cut -d":" -f 4 | sort -n | tail -n1`+1))
-echo; echo "Are you trying to spin up instance #$ID on port $(($ID+3305))?"
+echo; echo "Are you trying to spin up instance #$ID on port $ID?"
 echo "Speak now, or clean up after me later."
 if [[ $BYPASS != "Y" ]]
 then
@@ -16,7 +16,7 @@ then
   cp /etc/my.cnf /etc/my${ID}.cnf
   cp -r /etc/my.cnf.d /etc/my${ID}.cnf.d
   sed -i "/^!includedir/c !includedir /etc/my${ID}.cnf.d" /etc/my${ID}.cnf
-  sed -i "/^\[mysqld]/c \[mysqld]\nsocket\ =\ \/var\/lib\/mysql${ID}\/mysql\.sock\ndatadir\ =\ \/var\/lib\/mysql${ID}\/\ninnodb_data_home_dir\ =\ \/var\/lib\/mysql${ID}\/\ninnodb_log_group_home_dir\ =\ \/var\/lib\/mysql${ID}/\nport=$(($ID+3305))" /etc/my${ID}.cnf.d/server.cnf 
+  sed -i "/^\[mysqld]/c \[mysqld]\nsocket\ =\ \/var\/lib\/mysql${ID}\/mysql\.sock\ndatadir\ =\ \/var\/lib\/mysql${ID}\/\ninnodb_data_home_dir\ =\ \/var\/lib\/mysql${ID}\/\ninnodb_log_group_home_dir\ =\ \/var\/lib\/mysql${ID}/\nport=$ID" /etc/my${ID}.cnf.d/server.cnf 
   systemctl daemon-reload
   mkdir /var/lib/mysql${ID}
   chown mysql. /var/lib/mysql${ID}
